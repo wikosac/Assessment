f9dcec0ca9bb703ccf84c3be186833798e011de4
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-06-18 22:39:27 +0700

    Memproses input

--- app/src/main/java/org/d3if4401/assessment/ui/home/HomeFragment.kt
+++ app/src/main/java/org/d3if4401/assessment/ui/home/HomeFragment.kt
@@ -1,13 +1,15 @@
 package org.d3if4401.assessment.ui.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import org.d3if4401.assessment.R
 import org.d3if4401.assessment.databinding.HomeFragmentBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -26,8 +28,19 @@ class HomeFragment : Fragment() {
 
         binding.search.setOnClickListener {
             val res = "HASIL PENCARIAN: "
+            val kos = "HASIL TIDAK DITEMUKAN"
             val hasil = binding.bar.text.toString().uppercase()
-            binding.result.setText(res + hasil)
+            val forImg = binding.bar.text.toString().lowercase()
+            val imgRes = resources.getIdentifier(forImg, "drawable", "org.d3if4401.assessment")
+            val empty = requireContext()!!.resources.getDrawable(R.drawable.ic_launcher_foreground,requireContext()!!.theme)
+
+            if (imgRes > 0) {
+                binding.result.setText(res + hasil)
+                binding.imageView.setImageResource(imgRes)
+            } else {
+                binding.result.setText(kos)
+                binding.imageView.setImageDrawable(empty)
+            }
         }
         return binding.root
     }
