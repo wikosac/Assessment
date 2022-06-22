481458cede6646d7bc09869b8a8f973ac86c1ec5
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-06-23 05:17:43 +0700

    Menyimpan preferences dengan datastore

--- app/src/main/java/org/d3if4401/assessment/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/org/d3if4401/assessment/ui/gallery/GalleryFragment.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.view.*
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 import org.d3if4401.assessment.R
+import org.d3if4401.assessment.data.SettingDataStore
+import org.d3if4401.assessment.data.dataStore
 import org.d3if4401.assessment.databinding.GalleryFragmentBinding
 import org.d3if4401.assessment.databinding.HomeFragmentBinding
 import org.d3if4401.assessment.model.Hewan
@@ -27,6 +32,7 @@ class GalleryFragment : Fragment() {
     private lateinit var binding: GalleryFragmentBinding
     private lateinit var myAdapter: GalleryAdapter
     private var isLinearLayoutManager = true
+    private lateinit var layoutDataStore: SettingDataStore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,6 +56,15 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        layoutDataStore = SettingDataStore(requireContext().dataStore)
+        layoutDataStore.preferenceFlow.asLiveData()
+            .observe(viewLifecycleOwner, { value ->
+                isLinearLayoutManager = value
+                chooseLayout()
+                activity?.invalidateOptionsMenu()
+            })
+
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
@@ -90,6 +105,13 @@ class GalleryFragment : Fragment() {
             R.id.action_switch_layout -> {
 // Sets isLinearLayoutManager to the opposite value
                 isLinearLayoutManager = !isLinearLayoutManager
+
+                lifecycleScope.launch {
+                    layoutDataStore.saveLayoutToPreferencesStore(
+                        isLinearLayoutManager, requireContext()
+                    )
+                }
+
 // Sets layout and icon
                 chooseLayout()
                 setIcon(item)
